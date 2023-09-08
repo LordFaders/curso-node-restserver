32b1c0166e112638eb447cb3ae18c699f8abf499
Jaime Tapia <jaime.tapia.miquel@gmail.com>
2023-09-08 17:17:57 -0300

    Fin sección 11 - versión 4.0.0

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
-const { login } = require('../controllers/auth.controller');
+const { login, googleSignIn } = require('../controllers/auth.controller');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
@@ -14,6 +14,15 @@ router.post(
 		validarCampos,
 	],
 	login
+);
+
+router.post(
+	'/google',
+	[
+		check('id_token', 'id_token de google es necesario').not().isEmpty(),
+		validarCampos,
+	],
+	googleSignIn
 );
 
 module.exports = router;
